661e9232fc695fa89ee1676ca225d36d08095bf9
Tomab Scblieter <matt.meehan3@gmail.com>
2021-08-01 19:56:00 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Tomabs_Text_to_Speech
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer SpeechEngine;
+        private string LastText;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpeechEngine = new SpeechSynthesizer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeakStuff();
+        }
+
+        private void SpeakStuff()
+        {
+            SpeechEngine.SpeakAsync(textBox1.Text);
+            LastText = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                e.KeyChar = ' ';
+                SpeakStuff();
+            }
+            else if (e.KeyChar == (char)Keys.Up)
+            {
+                textBox1.Text = LastText;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Tomabs_Text_to_Speech
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
